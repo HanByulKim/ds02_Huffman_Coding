afadf8e4cbb864c1107974a55d69377b11e6ff1d
HanByulKim <rlagksquf30@gmail.com>
2016-11-02 22:39:28 +0900

    heap

---
+++ ds02/main.cpp
@@ -0,0 +1,103 @@
+/**
+*********************************************
+2016 Autumn DataStructure HW02
+SNU Dept. of Electrical & Computer Engineering
+2014-13261
+Han-Byul Kim
+*********************************************
+Rev. log.
+**/
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include "Heap.h"
+
+int* Rscript(int& cnt);
+std::string CleanLine(const std::string& n);
+
+void main(){
+	int* freq;
+	int cnt = 0;
+	freq = Rscript(cnt);
+	for (int i = 0; i < 30; i++)
+		std::cout << freq[i] << ", ";
+	Heap heap(cnt);
+	for (char i = 0; i < cnt; i++)
+		if(freq[i] != 0) heap.insert((char)(i + 97), freq[i]);
+	heap.print();
+
+	system("pause");
+}
+
+int* Rscript(int& cnt){
+	int* freq = new int[30]; for (int i = 0; i < 30; i++) { freq[i] = 0; }
+	std::string filename = "source.txt";
+	std::stringstream ss;
+	std::string str, cleanstr;
+
+	std::ifstream file(filename);
+
+	if (file){ // Same as Try Catch Exception
+		while (!file.eof()) {
+			std::getline(file, str);
+
+			// Clean each line.
+			cleanstr = CleanLine(str);
+
+			// Pass the cleaned-up string into a stringstream, to parse out the data elements.
+			ss.clear();
+			// Stringstream will parse directly on whitespace.
+
+			// 0~25 : a-z, 26 : space, 27 : comma, 28 : dot, 29 : new line
+			for (int i = 0; i < str.length(); i++){
+				switch (str.at(i)){
+				case ' ':
+					freq[26]++;
+					break;
+				case ',':
+					freq[27]++;
+					break;
+				case '.':
+					freq[28]++;
+					break;
+				case '\n':
+					freq[29]++;
+					break;
+				default:
+					freq[str.at(i) - 97]++;
+				}
+			}
+		}
+	}
+	else{ std::cout << "Wrong File!" << std::endl; }
+	for (int i = 0; i < 30; i++) if (freq[i] != 0)cnt++;
+
+	return freq;
+}
+
+std::string CleanLine(const std::string& n){
+	std::string cleanline;
+	char* char_line = (char*)n.c_str(); // Non-const cast required.
+
+	char* token = NULL;
+	char* context = NULL;
+	char  delims[] = " ,\t\n";
+
+	// During the first read, establish the char string and get the first token.
+	token = strtok_s(char_line, delims, &context);
+
+	// While there are any tokens left in "char_line"...
+	while (token != NULL)
+	{
+		// Accumulate the tokens.
+		cleanline += token;
+		cleanline += ' ';
+
+		// NOTE: NULL, function just re-uses the context after the first read.
+		token = strtok_s(NULL, delims, &context);
+	}
+
+	return cleanline;
+}
